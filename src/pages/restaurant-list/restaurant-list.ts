4654967b94596acf0ee9f2df3ab94688ada26e41
KyuTaeMoon <rbxo4456@naver.com>
2018-08-21 17:31:35 +0900

    0821waiting

--- src/pages/restaurant-list/restaurant-list.ts
+++ src/pages/restaurant-list/restaurant-list.ts
@@ -30,6 +30,7 @@ export class RestaurantListPage {
 	public table: any;
 	public menuCollection: any;
 	public  db = firebase.firestore();
+	waitingNumber=0;
 	owner:string='';
 	order:string='';
 	total:number=0;
@@ -45,70 +46,76 @@ export class RestaurantListPage {
     markersGroup;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public service: RestaurantService, public toastCtrl: ToastController, public modalCtrl: ModalController, public config: Config, private alertCtrl: AlertController,  private http: HttpClient) {
-        this.proptype = this.navParams.get('proptype') || "";
-        this.from = this.navParams.get('from') || "";
+        this.store = this.navParams.get('store_code');
+        this.start();
         // console.log(this.proptype);
     }
+    start(){
+		var abc =this.startAsync().then(num => this.waitingNumber = num);
+	}
+	waiting(){
+		var abc =this.checkoutAsync().then(num => this.waitingNumber = num);
+
+		// var success  = this.checkoutAsync().then(()=>{this.registerOrder();}).then(()=>{this.navCtrl.push('page-home');}).catch();
+	}
+	// start(){
+	// 	var success  = this.checkoutAsync().then(()=>{this.registerOrder();}).then(()=>{this.navCtrl.push('page-home');}).catch();
+	// }
+	// registerOrder(){
+	// 	console.log(this.owner)
+    //
+	// 	let body = {
+	// 		"notification":{
+	// 			"title":"New waiting is arrived",
+	// 			"body":this.order,
+	// 			"sound":"default",
+	// 			"click_action":"FCM_PLUGIN_ACTIVITY",
+	// 			"icon":"fcm_push_icon"
+	// 		},
+	// 		"data":{
+    //
+	// 		},
+	// 		"to":"/topics/"+this.store,
+	// 		"priority":"high",
+	// 		"restricted_package_name":""
+	// 	}
+	// 	let options = new HttpHeaders().set('Content-Type','application/json');
+	// 	this.http.post("https://fcm.googleapis.com/fcm/send",body,{
+	// 		headers: options.set('Authorization', 'key=AAAA94sqthU:APA91bF4quIXvQYLJlwp3mNMh6HdYpTGoDIIVOODLheD5LcLdge-JZhe4N2AaQjVMtqwDdQGhaXW4BMhkpEW9SuTwYWBuASd1bZGSaB_Me9sw3cCcUNlYa7NetC-BkX5OaBsLqFEJgRC'),
+	// 	})
+	// 		.subscribe();
+	// }
+
 	async checkoutAsync(){
 		let check = await this._check();
 		return check;
 	}
-	start(){
-		var success  = this.checkoutAsync().then(()=>{this.registerOrder();}).then(()=>{this.navCtrl.push('page-home');}).catch();
+	async startAsync(){
+		let check = await this._start();
+		return check;
 	}
-	registerOrder(){
-		console.log(this.owner)
-
-		let body = {
-			"notification":{
-				"title":"New waiting is arrived",
-				"body":this.order,
-				"sound":"default",
-				"click_action":"FCM_PLUGIN_ACTIVITY",
-				"icon":"fcm_push_icon"
-			},
-			"data":{
-
-			},
-			"to":"/topics/"+this.store,
-			"priority":"high",
-			"restricted_package_name":""
-		}
-		let options = new HttpHeaders().set('Content-Type','application/json');
-		this.http.post("https://fcm.googleapis.com/fcm/send",body,{
-			headers: options.set('Authorization', 'key=AAAA94sqthU:APA91bF4quIXvQYLJlwp3mNMh6HdYpTGoDIIVOODLheD5LcLdge-JZhe4N2AaQjVMtqwDdQGhaXW4BMhkpEW9SuTwYWBuASd1bZGSaB_Me9sw3cCcUNlYa7NetC-BkX5OaBsLqFEJgRC'),
-		})
-			.subscribe();
-	}
-
 	_check():Promise<any>{
 		return new Promise<any>(resolve => {
-			var success = "success";
-			this.date = new Date().toUTCString();
-			var addDoc = this.db.collection('waiting').add({
-
-				status : true,
-				table_num : this.table,
-				timestamp : this.date,
-				user : firebase.auth().currentUser.email,
-				store_code : this.store,
-				id:""
-
-			}).then(function(docRef) {
-
-				console.log("Document written with ID: ", docRef.id);
-				//	var docid = docRef.id.toString()
-				var setid = docRef.update({
-					id: docRef.id.toString()
-				})
-				resolve(success);
-
-				console.log('Added document');
+			var wn = 0;
+			var addDoc = this.db.collection(this.store).add({
+					user: firebase.auth().currentUser.email
+				}
+			).then(success => {
+				this.db.collection(this.store).get().then(function(querySnapshot) {
+					resolve(querySnapshot.size);
+				});
 			});
-
-			//   resolve(store);
 		})
 	}
+	_start():Promise<any>{
+		return new Promise<any>(resolve => {
+			var wn = 0;
+			this.db.collection(this.store).get().then(function(querySnapshot) {
+				resolve(querySnapshot.size);
+			});
+		})
+	}
+
 
 
 }
